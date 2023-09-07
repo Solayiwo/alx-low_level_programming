a50b00544ffe8c16cb931ddf82b9176b4cbd922b
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-07 12:56:01 -0700

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
-  main - Entry point
- 
-  Description: A C program that prints with put function
- 
-  Return: Always 0 (Success)
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
