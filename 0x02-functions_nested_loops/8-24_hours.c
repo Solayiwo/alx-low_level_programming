9c24a5ec9b8ce357bcb9e967cdc9141e60754f7e
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-13 12:00:57 -0700

    Longest day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: function that prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ * using int _isalpha(int c) prototype
+ *
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
