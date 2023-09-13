13a3dc56f8f9184f1083f84346e28a08644fdbad
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-13 14:52:09 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: function that that adds two integers
+ * and returns the result int add(int, int) prototype
+ *
+ *@a: Fisrt parameter to be checked
+ *@b: Seond parameter to be checked
+ *
+ * Return: The sum of the two integers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
