31d662d9845e985bd2796cb51463a62d1129af50
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-14 15:51:14 -0700

    Parameter reviewed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,6 @@
  *
  * Description: Function that checks for uppercase character
  * using int _isupper(int c) prototype.
- * 
  * @c: Parameter to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise.
