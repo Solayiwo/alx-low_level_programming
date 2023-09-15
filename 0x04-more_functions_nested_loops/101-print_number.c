8d9123f32d2aabf885a204cee96e950c0d1b2d71
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-15 02:24:50 -0700

    Numbers have meaning

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * Description: Function that prints an integer using _putchar function
+ * and void print_number(int n) prototype.
+ *@n: Parameter to be checked
+ *
+ * Return: void
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
