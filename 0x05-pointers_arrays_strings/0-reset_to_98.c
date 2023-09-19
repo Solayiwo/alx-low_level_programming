f7df0ff574a5326bc7aedaebf66099c963f73cd0
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2023-09-18 19:09:39 -0700

    Pointer update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * description: Takes a pointer to an int as a parameter
+ * and update the value to 98
+ * @n: type int pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int i = 98;
+	*n = i;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project on C - Pointers, arrays and strings
