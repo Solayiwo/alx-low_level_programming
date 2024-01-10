b75d4ce67887f0dfeca7df111171780fc53424ed
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-10 15:23:24 -0800

    Unforgettable truth

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * Description: function that prints a string
+ * @s: string to print
+ * Return: void.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
