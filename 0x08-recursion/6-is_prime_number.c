6ac91704246fa14a5ba835d439507f56ee010412
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-10 18:27:20 -0800

    Inception successful

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _prime_checker - Entry point
+ * Description: checks recursively the input from is_prime_number
+ * @n: iterator
+ * @base: parameter to check
+ * Return: 1 if n is a prime, else return 0 otherwise
+ */
+
+int _prime_checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (_prime_checker(n + 1, base));
+	return (1);
+}
+/**
+  * is_prime_number - checks if the number is a prime number
+  * @n: the number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+
+int is_prime_number(int n)
+{
+	return (_prime_checker(2, n));
+}
