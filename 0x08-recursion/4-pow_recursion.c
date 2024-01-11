13803c5633781a9c44e907b28d33c5721962c68c
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-10 17:29:13 -0800

    Power of ideals

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * Description: function that returns the value of x raised to the power of y
+ * @x: base
+ * @y: power
+ * Return: value of the exponentiation.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
