f00d2d534be84f21e8a25f6d2215c28794ee915a
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-13 00:23:19 -0800

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			if (s1[i] < s2[i])
 				return (s1[i] - s2[i]);
-			else if (s2[i] - s2[i])
+			else if (s2[i] - s1[i])
 				return (s1[i] - s2[i]);
 		}
 	}
