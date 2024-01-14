a65b060f4fa461a75c51cb22c5a230b758616f6b
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-13 19:22:48 -0800

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * Description: Function that prints the chessboard
+ * @a: pointer to check
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - j]);
+	}
+	_putchar('\n');
+}
