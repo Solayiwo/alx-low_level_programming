9b6d4c39192d92149520f04ec18c917b17dfad4f
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-14 16:47:57 -0800

    Static libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * Description: Returns the lenght of a string
+ * @s: Type char pointer
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+
+	return (c);
+}
