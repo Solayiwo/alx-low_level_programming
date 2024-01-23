3a7db44bafaa7a09bfe8a1ab5b0b1a261e34fca2
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-01-23 15:49:31 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
