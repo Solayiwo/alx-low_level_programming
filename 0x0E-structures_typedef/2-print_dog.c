eb114a1f009b544cd8ed40614beb33a8fa4cc03a
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-02-06 16:15:36 -0800

    Love is unconditional

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("Name: %f\n", (d->age) ? d->age : 0);
-		printf("Name: %s\n", (d->owner) ? d->owner : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
