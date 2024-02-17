8f9e9a5ad3755266a34de6b04af2d69833425056
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-02-16 18:23:19 -0800

    Append gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,8 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
+	{
 		for (nlength = 0; text_content[nlength];)
 			nlength++;
+	}
 
 	fd = open(filename, O_APPEND | O_WRONLY);
 	wr = write(fd, text_content, nlength);
