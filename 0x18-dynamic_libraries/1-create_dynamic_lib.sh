fd80ff33ac4de74fcc1857ffc315de5a1e2a8f31
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-03-25 10:42:24 -0700

    Dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC functions*.c
-gcc -shared -o liball.so *.o
-eport LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
